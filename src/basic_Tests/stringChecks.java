83c3825f0ffce5a61b6d45cebf986c70cd12bdef
Sounak Nandi <sounak_nandi@epam.com>
2022-11-10 13:49:44 +0530

    Tests string-1

--- src/basic_Tests/stringChecks.java
+++ src/basic_Tests/stringChecks.java
@@ -10,46 +10,49 @@ import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
 public class stringChecks {
-	
-	static String x = "II SO  Tel me that";
+
+	static String x = "gt To ko";
 
 	static String[] xx = x.split(" ");
-	public static void main(String args[]) {	
-		
-		  String s = ""; 
-		  char[] ff = x.toCharArray();
-		  Map<Object,Integer> f = new HashMap<Object, Integer>();
-		  List<char[]> asList = Arrays.asList(ff); 
-		  
-		  System.out.print(asList.get(0));
-		  
-		List<String> cc = x.chars().mapToObj(d->Character.toString(d)).collect(Collectors.toList());
-		
+
+	public static void main(String args[]) {
+
+		String s = "";
+		char[] ff = x.toCharArray();
+		Map<Object, Integer> f = new HashMap<Object, Integer>();
+
+		List<Character> cc = x.chars().mapToObj(d -> (char)(d)).collect(Collectors.toList());
+List<char[]> chr = new ArrayList<>(Arrays.asList(x.toCharArray()));
+
 		List<Character> listC = new ArrayList<Character>();
-	    for (char c : ff) {
-	        listC.add(c);
-	    }
-		  x.chars().forEach(d->{
-			  if(f.containsKey(Character.toString(d))) {
-				  f.put(Character.toString(d),f.get(Character.toString(d))+1);
-			  }else {
-				  f.put(Character.toString(d), 1);
-			  }
-		  });
-		  
-		  
-		  System.out.print(f);
-			/*
-			 * ArrayList<String> dd = new ArrayList<String>(Arrays.asList(xx));
-			 * dd.stream().forEach(x->{ char[] xy = x.toCharArray(); for(int
-			 * o=0;o<xy.length;o++) { if(o==0) {
-			 * System.out.print(Character.toUpperCase(xy[o])); }else {
-			 * System.out.print(xy[o]); }
-			 * 
-			 * } System.out.print(" "); });
-			 */
+		for (char c : ff) {
+			listC.add(c);
+		}
+
+		x.chars().forEach(d -> {
+			System.out.print(d);
+		});
+
+		x.chars().forEach(d -> {
+			if (f.containsKey((char)d)) {
+				f.put((char)d, f.get((char)d) + 1);
+			} else {
+				f.put((char)d, 1);
+			}
+		});
+
+		System.out.print(f);
+
+		/*
+		 * ArrayList<String> dd = new ArrayList<String>(Arrays.asList(xx));
+		 * dd.stream().forEach(x->{ char[] xy = x.toCharArray(); for(int
+		 * o=0;o<xy.length;o++) { if(o==0) {
+		 * System.out.print(Character.toUpperCase(xy[o])); }else {
+		 * System.out.print(xy[o]); }
+		 * 
+		 * } System.out.print(" "); });
+		 */
 
 	}
-
 
 }
